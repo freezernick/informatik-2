2b33c5b50395661032f6aa7b14ee880e3c437d6a
FreezerNick <nl@f2p-entertainment.de>
2020-02-02 13:25:55 +0100

    Comments, .exe validation

--- Program.cs
+++ Program.cs
@@ -15,12 +15,12 @@ namespace GameMaster
             Version = 1;
         }
 
-        /**
-         * Populates the members of a game instance with the valus of the specified config file
-         * @param GameObject The game object that should be initialized
-         * @param config The config file holding the information
-         * @return The populated game object
-         */
+        /// <summary>
+        /// Populates the member variables of a specified game with values of the specified config file
+        /// </summary>
+        /// <param name="GameObject">The game object that should be populated</param>
+        /// <param name="config">The config file holding the values</param>
+        /// <returns>The populated game object</returns>
         public static Game ConfigToGame(Game GameObject, DataFile config)
         {
             GameObject.Name = config.Get<string>("Name");
@@ -35,9 +35,9 @@ namespace GameMaster
             return GameObject;
         }
 
-        /**
-         * Saves the member variables of the game to the associated config file
-         */
+        /// <summary>
+        /// Saves the member variables of the game to the associated config file
+        /// </summary>
         public void Save()
         {
             AssociatedConfig.Set<string>("Name", Name);
@@ -51,23 +51,22 @@ namespace GameMaster
             AssociatedConfig.SaveAllData();
         }
 
-        /**
-         * Creates a new config file in a new folder based on the game's id
-         */
+        /// <summary>
+        /// Creates a new config file for the specified ID
+        /// </summary>
         public void CreateNew()
         {
             AssociatedConfig = new DataFile(Path.Combine(AppContext.BaseDirectory + @"\rulesets\", ID) + @"\ruleset");
             Save();
         }
 
-        /**
-         * Checks if the executable specified in the StartAction property is valid
-         * TODO: Check if .exe
-         * @return Whether the start action is valid or not
-         */
+        /// <summary>
+        /// Checks if the executable specified in the StartAction property is valid
+        /// </summary>
+        /// <returns>Whether the start action is valid or not</returns>
         public bool ValidAction()
         {
-            if(File.Exists(StartAction))
+            if(File.Exists(StartAction) && StartAction.EndsWith(".exe"))
             {
                 return true;
             }
@@ -91,7 +90,7 @@ namespace GameMaster
     static class Program
     {
         /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
