e7c478365f84d11f4cfadfb4c61727c19e3970f3
FreezerNick <nl@f2p-entertainment.de>
2020-02-20 14:22:12 +0100

    Change Class Structure

--- GameMaster/Process.cs
+++ GameMaster/Process.cs
@@ -1,4 +1,15 @@
-﻿namespace GameMaster
+﻿using GameMaster.Abstracts;
+using GameMaster.Templates;
+
+namespace GameMaster
 {
-    
+    public class GM_Process
+    {
+        public static Template CurrentTemplate;
+        public static World CurrentWorld;
+        public GM_Process()
+        {
+
+        }
+    }
 }
--- GameMaster/Template.cs
+++ GameMaster/Template.cs
@@ -1,100 +1,18 @@
-﻿namespace GameMaster
+﻿using System.Collections;
+using GameMaster.Abstracts;
+
+namespace GameMaster.Templates
 {
-    public class World
-    {
-        public string Name;
-        public Origin CoordinateOrigin;
-    }
-
-    public class Trigger
-    {
-        public string Name;
-        public bool Unique;
-    }
-
-    public class Action
-    {
-        public string Name;
-    }
-
-    public struct ScreenLocation
-    {
-        public int X;
-        public int Y;
-    }
-
-    public class UIParameter
-    {
-        public string Name;
-        public ScreenLocation ScreenLocation;
-    }
-
-    public class WorldParameter : WorldObject
-    {
-        public string Name;
-        public Action ToDo;
-        public ScreenLocation ScreenLocation;
-    }
-
-    public class Player : WorldParameter
-    {
-        public Player()
-        {
-            Name = "Player";
-        }
-    }
-
-    public class Goal : WorldParameter
-    {
-        public Goal()
-        {
-            Name = "Goal";
-        }
-    }
-
-    public struct Location
-    {
-        public float X;
-        public float Y;
-        public float Z;
-
-        public Location(float x = 0.0f, float y = 0.0f, float z = 0.0f)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-        }
-    }
-
-    public struct Rotation
-    {
-        public float Roll;
-        public float Yaw;
-        public float Pitch;
-
-        public Rotation(float yaw = 0.0f, float pitch = 0.0f, float roll = 0.0f)
-        {
-            Roll = roll;
-            Yaw = yaw;
-            Pitch = pitch;
-        }
-    }
-
-    public class WorldObject
-    {
-        public Location WorldLocation;
-        public Rotation WorldRotation;
-    }
-
-    public class Origin : WorldObject
-    {
-
-    }
-
     public class Template
     {
         public string Name;
         protected bool ThirdDimension;
+        public ArrayList Worlds;
+
+        public Template()
+        {
+            Worlds = new ArrayList();
+        }
     }
     
     public class Template3D : Template
@@ -115,6 +33,11 @@
 
     public class PlattformerTemplate : Template2D
     {
-        public Player PlayerObject;
+        public GameWorld.Player PlayerObject;
+        public GameWorld.Goal GoalObject;
+        public PlattformerTemplate()
+        {
+
+        }
     }
 }
