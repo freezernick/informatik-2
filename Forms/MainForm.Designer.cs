c7f06d9b2f469f546a7ff62db0590b12ec9f5ffc
FreezerNick <nl@f2p-entertainment.de>
2020-02-20 16:21:46 +0100

    Small Design Changes

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -30,12 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btStart = new System.Windows.Forms.Button();
             this.btAdd = new System.Windows.Forms.Button();
-            this.btEditProp = new System.Windows.Forms.Button();
             this.btNew = new System.Windows.Forms.Button();
-            this.btEditRules = new System.Windows.Forms.Button();
             this.lName = new System.Windows.Forms.Label();
             this.lCreator = new System.Windows.Forms.Label();
             this.lLastChanged = new System.Windows.Forms.Label();
@@ -45,7 +41,9 @@
             this.lVersionValue = new System.Windows.Forms.Label();
             this.lLastChangedValue = new System.Windows.Forms.Label();
             this.Tray = new System.Windows.Forms.NotifyIcon(this.components);
-            this.groupBox1.SuspendLayout();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btEditProp = new System.Windows.Forms.Button();
+            this.btEditRules = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -58,32 +56,6 @@
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.btStart);
-            this.groupBox1.Controls.Add(this.btEditProp);
-            this.groupBox1.Controls.Add(this.btEditRules);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.groupBox1.Location = new System.Drawing.Point(310, 124);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(272, 111);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            // 
-            // btStart
-            // 
-            this.btStart.Enabled = false;
-            this.btStart.Location = new System.Drawing.Point(4, 72);
-            this.btStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btStart.Name = "btStart";
-            this.btStart.Size = new System.Drawing.Size(264, 22);
-            this.btStart.TabIndex = 6;
-            this.btStart.Text = "Start";
-            this.btStart.UseVisualStyleBackColor = true;
-            this.btStart.Click += new System.EventHandler(this.btStart_Click);
-            // 
             // btAdd
             // 
             this.btAdd.Location = new System.Drawing.Point(11, 213);
@@ -95,19 +67,6 @@
             this.btAdd.UseVisualStyleBackColor = true;
             this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
             // 
-            // btEditProp
-            // 
-            this.btEditProp.Enabled = false;
-            this.btEditProp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.btEditProp.Location = new System.Drawing.Point(4, 20);
-            this.btEditProp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btEditProp.Name = "btEditProp";
-            this.btEditProp.Size = new System.Drawing.Size(264, 22);
-            this.btEditProp.TabIndex = 4;
-            this.btEditProp.Text = "Edit properties";
-            this.btEditProp.UseVisualStyleBackColor = true;
-            this.btEditProp.Click += new System.EventHandler(this.btEditProp_Click);
-            // 
             // btNew
             // 
             this.btNew.Location = new System.Drawing.Point(190, 213);
@@ -118,19 +77,6 @@
             this.btNew.Text = "New";
             this.btNew.UseVisualStyleBackColor = true;
             this.btNew.Click += new System.EventHandler(this.btNew_Click);
-            // 
-            // btEditRules
-            // 
-            this.btEditRules.Enabled = false;
-            this.btEditRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.btEditRules.Location = new System.Drawing.Point(4, 46);
-            this.btEditRules.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btEditRules.Name = "btEditRules";
-            this.btEditRules.Size = new System.Drawing.Size(264, 22);
-            this.btEditRules.TabIndex = 2;
-            this.btEditRules.Text = "Edit rules";
-            this.btEditRules.UseVisualStyleBackColor = true;
-            this.btEditRules.Click += new System.EventHandler(this.btEditRules_Click);
             // 
             // lName
             // 
@@ -225,11 +171,49 @@
             this.Tray.Text = "GameMaster";
             this.Tray.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Tray_MouseDoubleClick);
             // 
+            // btStart
+            // 
+            this.btStart.Enabled = false;
+            this.btStart.Location = new System.Drawing.Point(310, 212);
+            this.btStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(279, 22);
+            this.btStart.TabIndex = 12;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            // 
+            // btEditProp
+            // 
+            this.btEditProp.Enabled = false;
+            this.btEditProp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btEditProp.Location = new System.Drawing.Point(310, 160);
+            this.btEditProp.Margin = new System.Windows.Forms.Padding(2);
+            this.btEditProp.Name = "btEditProp";
+            this.btEditProp.Size = new System.Drawing.Size(279, 22);
+            this.btEditProp.TabIndex = 11;
+            this.btEditProp.Text = "Edit properties";
+            this.btEditProp.UseVisualStyleBackColor = true;
+            // 
+            // btEditRules
+            // 
+            this.btEditRules.Enabled = false;
+            this.btEditRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btEditRules.Location = new System.Drawing.Point(310, 186);
+            this.btEditRules.Margin = new System.Windows.Forms.Padding(2);
+            this.btEditRules.Name = "btEditRules";
+            this.btEditRules.Size = new System.Drawing.Size(279, 22);
+            this.btEditRules.TabIndex = 10;
+            this.btEditRules.Text = "Edit rules";
+            this.btEditRules.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 245);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.btEditProp);
+            this.Controls.Add(this.btEditRules);
             this.Controls.Add(this.lLastChangedValue);
             this.Controls.Add(this.btAdd);
             this.Controls.Add(this.btNew);
@@ -240,7 +224,6 @@
             this.Controls.Add(this.lLastChanged);
             this.Controls.Add(this.lCreator);
             this.Controls.Add(this.lName);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listBox1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
@@ -249,7 +232,6 @@
             this.ShowIcon = false;
             this.Text = "Game Master";
             this.Load += new System.EventHandler(this.lErscheinungsdatum_Load);
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,21 +240,20 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btEditRules;
         private System.Windows.Forms.Button btAdd;
-        private System.Windows.Forms.Button btEditProp;
         private System.Windows.Forms.Button btNew;
         private System.Windows.Forms.Label lName;
         private System.Windows.Forms.Label lCreator;
         private System.Windows.Forms.Label lLastChanged;
         private System.Windows.Forms.Label lVersion;
-        private System.Windows.Forms.Button btStart;
         private System.Windows.Forms.Label lNameValue;
         private System.Windows.Forms.Label lAuthorValue;
         private System.Windows.Forms.Label lVersionValue;
         private System.Windows.Forms.Label lLastChangedValue;
         private System.Windows.Forms.NotifyIcon Tray;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btEditProp;
+        private System.Windows.Forms.Button btEditRules;
     }
 }
 
